0dd96cbc1ad5dd6f72f1ae6d7ccb03b912796cc2
AtropsCooper <qwqw1212cn@163.com>
2023-01-02 01:44:30 +0900

    Higher Attack Speed

--- Component/AttackComponent.h
+++ Component/AttackComponent.h
@@ -4,7 +4,7 @@
 class AttackComponent : public Component
 {
 public:
-    AttackComponent(class Entity* owner, int order) : Component(owner, order), interval(0.5f), cooldown(0.0f) {}
+    AttackComponent(class Entity* owner, int order) : Component(owner, order), interval(0.4f), cooldown(0.0f) {}
 
     float interval; // How long does an attack take
     float cooldown; // How long until next attack
